3320733a6d0f067b1c3e9e9ea8b12b3235a6dd1f
yefer <yefer@DESKTOP-UU642SQ>
2016-12-11 18:13:26 -0500

    carga de  2  clases de  entidades de  clientes y producctos

---
+++ src/co/appreactor/facturador/modelo/entidades/Cliente.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.appreactor.facturador.modelo.entidades;
+
+/**
+ *
+ * @author yefer
+ */
+public class Cliente {
+    
+   private Long  idCliente;
+   private String nombre;
+   private String correo;
+   private String telefono;
+   private String direccion;
+   private boolean estado;
+  // private Tipo_cliente id_Tipo_Cliente;
+   
+    public Cliente (){
+        
+    }
+    
+    public Cliente (Long idCliente){
+        this.idCliente=idCliente;
+    }
+
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+   
+    
+   
+}
---
+++ src/co/appreactor/facturador/modelo/entidades/Producto.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.appreactor.facturador.modelo.entidades;
+
+/**
+ *
+ * @author yefer
+ */
+public class Producto {
+    
+    private Long idProducto;
+    private String nombre;
+    private double precioUnitario;
+    private Integer cantidad;
+    private boolean estado;
+    
+    public Producto (){
+        
+    }
+    
+    public Producto (Long idProducto){
+        
+        this.idProducto=idProducto;
+    }
+
+    public Long getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Long idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+}
